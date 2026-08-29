76b6cafe187c84718b3d3b076a8216790f806d8c
Margarita Parhomenko <margarita.parhomenko1@gmail.com>
2026-08-29 22:39:52 +0300

    use scalar instead of swagger

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -1,26 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// 1. Services Configuration
+builder.Services.AddOpenApi();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<ElkaroContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Build application (Must only be called ONCE)
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. HTTP Pipeline Configuration
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference(); // Accessible at /scalar/v1
 }
 
 app.UseHttpsRedirection();
 
+// 3. API Endpoints
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -28,7 +31,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
